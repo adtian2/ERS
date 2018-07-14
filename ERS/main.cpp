5e916425042100d3ff6a55992de6c4266e62ad1b
Andrew Tian <AndrewTian@andrews-macbook-pro.local>
2018-07-14 11:55:17 -0500

    Initial Commit

---
+++ ERS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ERS
+//
+//  Created by Andrew Tian on 7/14/18.
+//  Copyright © 2018 Andrew Tian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
